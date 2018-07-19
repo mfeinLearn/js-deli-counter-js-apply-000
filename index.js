00c37742124b9af8af42a8919df3e9bafc5875bb
Malcome Fein <mfein90@gmail.com>
2018-07-19 08:05:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ function takeANumber(katzDeliLine, newPersonName) {
   return "Welcome, " + newPersonName + ". You are number " + katzDeliLine.length + " in line.";
 }
 
+  var i = 0;
+function takeANumberOldSchool(){
+  i++;
+    return "Welcome You are number " + i;
+  
+}
+
+//it works now:
+//takeANumber() Welcome Humzah You are number 1 in line
+//takeANumber() Welcome Malcome You are number 2 in line
+//nowServing()
+//nowServing()
+//takeANumber() Welcome Joe You are number 1 in line
+
+//way we want it to work
+//takeANumberOldSchool() Welcome  You are number 1 
+//takeANumberOldSchool() Welcome You are number 2 
+//nowServing()
+//nowServing()
+//takeANumberOldSchool() Welcome you are number 3
+
+
+
+
+
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
        return ("Currently serving " + katzDeliLine.shift() + ".");
